4dc315ba3a54df1ac1bcf383a4db0628b3cdc9ae
Rati <rati@Krishnas-iMac.local>
2017-06-16 15:28:24 +0530

    First Commit

---
+++ PRAddLayer.swift
@@ -0,0 +1,28 @@
+//
+//  PRAddLayer.swift
+//
+//  Created by Ratti on 22/12/16.
+//  Copyright © 2016 Ratti. All rights reserved.
+//
+
+import UIKit
+
+extension UITextField{
+    
+    static func applyLayer(textField : UITextField ,color : UIColor , placeHolder : String)
+    {
+        
+        let layer : CALayer = CALayer.init()
+        layer.frame = CGRect.init(x: 0, y: textField.frame.size.height - 5, width: textField.frame.size.width, height: 0.5)
+        layer.backgroundColor = color.cgColor
+        textField.layer.addSublayer(layer);
+        //textField.attributedPlaceholder = NSAttributedString(string: placeHolder, attributes: [NSForegroundColorAttributeName : color])
+        
+    }
+    
+}
+
+class PRAddLayer: NSObject {
+    
+
+}
